509e62fa9dcba2a1961d055f4a64012819ad5b8e
Inuasuraz <kiatsiri_4046@hotmail.co.th>
2019-08-21 18:41:48 +0700

    TASK 2 : UI & Start using Binding

--- app/src/main/java/buu/informatics/s59160081/parkinglot/MainActivity.kt
+++ app/src/main/java/buu/informatics/s59160081/parkinglot/MainActivity.kt
@@ -2,11 +2,27 @@ package buu.informatics.s59160081.parkinglot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Button_One.setOnClickListener{
+
+        }
+
+        Button_Two.setOnClickListener{
+
+        }
+
+        Button_Three.setOnClickListener{
+
+        }
+
     }
+
+
 }
